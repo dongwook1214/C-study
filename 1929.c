e22094c1a4559d0271ae020225696ede6180c81e
dongwook <dongwook1214@gmail.com>
2022-01-14 21:38:39 +0900

    baekjoon 1929 retry and success

--- 1929.c
+++ 1929.c
@@ -10,23 +10,48 @@ int distinguish(num){
     return 1;
 }
 
+int distinguish2(int num,int* list,int d){
+    int i;
+
+    for(i = 0;i <= d; i++){
+        if(list[i]>=num){
+            break;
+        }
+        if(num % list[i] == 0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
 int main(){
-    int a,b;
+    int a,b,d,i,j;
+    int list[1000];
+    int c = 0;
+
     scanf("%d %d",&a,&b);
 
-    if(a==1){
+    for (i = 2; i <= 1000; i++){
+        if(distinguish(i)==1){
+            list[c] = i;
+            c++;
+        }
+    }
+    d = c - 1;
+
+    if(a == 1){
         a++;
         for(; a <= b; a++){
-            if(distinguish(a)==1){
+            if(distinguish2(a,list,d)==1){
                 printf("%d\n",a);
             }
         }
     }else{
         for(; a <= b; a++){
-            if(distinguish(a)==1){
+            if(distinguish2(a,list,d)==1){
                 printf("%d\n",a);
-        
             }
         }
     }
+    
 }
