9b43daf450aaca8cf6ddbe9328bbd95cf0acad25
dongwook <dongwook1214@gmail.com>
2022-03-06 22:27:24 +0900

    baekjoon 1149 fail

---
+++ 1149.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int color[1000][3];
+    int num,i,j,k;
+    int min=2100000000;
+    int dp[1000] = {0,};
+    scanf("%d",&num);
+    for(i = 0; i < num; i++){
+        scanf("%d %d %d",&color[i][0],&color[i][0],&color[i][0]);
+    }
+
+
+    
+}
