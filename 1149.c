02fca11628c3e8961beca2f9efef4fbdd89aa61a
dongwook <dongwook1214@gmail.com>
2022-03-10 00:01:26 +0900

    baekjoon 1149

--- 1149.c
+++ 1149.c
@@ -1,14 +1,44 @@
 #include<stdio.h>
+int color[1000][3];
+
+void dp(int i){
+    int min;
+    min = color[i-1][1];
+    if(min > color[i-1][2]){
+        min = color[i-1][2];
+    }
+    color[i][0] += min; 
+    min = color[i-1][0];
+    if(min > color[i-1][2]){
+        min = color[i-1][2];
+    }
+    color[i][1] += min;
+    min = color[i-1][0];
+    if(min > color[i-1][1]){
+        min = color[i-1][1];
+    }
+    color[i][2] += min;
+}
+
 int main(){
-    int color[1000][3];
-    int num,i,j,k;
-    int min=2100000000;
-    int dp[1000] = {0,};
+    
+    int num,i,min;
+
     scanf("%d",&num);
     for(i = 0; i < num; i++){
-        scanf("%d %d %d",&color[i][0],&color[i][0],&color[i][0]);
+        scanf("%d %d %d",&color[i][0],&color[i][1],&color[i][2]);
     }
 
-
+    for(i = 1; i < num; i++){
+        dp(i);
+    }
+    min = color[num-1][0];
+    if(color[num-1][1]<min){
+        min = color[num-1][1];
+    }
+    if(min>color[num-1][2]){
+        min = color[num-1][2];
+    }
+    printf("%d",min);
     
 }
